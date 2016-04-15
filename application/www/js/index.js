63f1d0717487a89e298dd5d74baedf433af3af72
Chris Vuletich <cvuletich@gmail.com>
2016-04-14 22:50:23 -0500

    Video downloads do work!!!

--- application/www/js/index.js
+++ application/www/js/index.js
@@ -23,8 +23,7 @@ function jsonpCallback(response) {
       data.response[key].filepath = downloadFile(key + 1);
     });
   }
-  localStorage.setItem('data', JSON.stringify(data));
-  document.location = 'main.html';
+  window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 }
 
 function pause(count) {
@@ -41,26 +40,6 @@ function onDeviceReady() {
   $('.title-bar').css({
     'top' : $('.blue-bar').offset().top + 10
   });
-  //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
-/*
-  var fileTransfer = new FileTransfer();
-  var uri = encodeURI("http://clients.voltology.io/cars.com/sellandtrade/video/chapter1.mp4");
-  fileTransfer.download(
-      uri,
-      cordova.file.applicationStorageDirectory + 'Documents',
-      function(entry) {
-          alert("download complete: " + entry.fullPath);
-      },
-      function(error) {
-          alert("download error source " + error.source);
-          alert("download error target " + error.target);
-          alert("upload error code" + error.code);
-      },
-      false,
-      {
-      }
-  );
-*/
   var networkState = navigator.connection.type;
   if (networkState === 'wifi') {
     getData();
@@ -68,38 +47,41 @@ function onDeviceReady() {
     alert('You must be connected to the internet when running this application for the first time. Please connect to WiFi and reopen the application.');
     navigator.app.exitApp();
   } else {
-    document.location = 'main.html';
+    //document.location = 'main.html';
   }
 }
 
 
 
 
+var downloadCount = 0;
 function gotFS(fileSystem) {
-    fileSystem.root.getDirectory("vids", {create: true}, gotDir);
+  fileSystem.root.getDirectory("vids", {create: true}, gotDir);
 }
 
 function gotDir(dirEntry) {
-    dirEntry.getFile("chapter1.mp4", {create: true, exclusive: false}, gotFile);
+  dirEntry.getFile("chapter" + downloadCount + ".mp4", {create: true, exclusive: false}, gotFile);
 }
 
 function gotFile(fileEntry) {
     var localPath = fileEntry.fullPath;
     var localUrl = fileEntry.toURL();
 
-    alert('Loaded local path: ' + localPath);
-    alert('Loaded local url: ' + localUrl);
+    //alert('Loaded local path: ' + localPath);
+    //alert('Loaded local url: ' + localUrl);
 
     var fileTransfer = new FileTransfer();
-    var uri = encodeURI('http://dealeradvantage.cars.com/landing/2016.03-Sell-and-Trade-Center/2016.03-st-resolution/video/chapter1.mp4');
-    alert('Downloading ' + uri + ' to ' + localPath);
+    //var uri = encodeURI('http://dealeradvantage.cars.com/landing/2016.03-Sell-and-Trade-Center/2016.03-st-resolution/video/chapter' + downloadCount + '.mp4');
+    var uri = encodeURI(data.response[downloadCount].filepath);
+    $('.loading-text').html('Retrieving Files... (' + (downloadCount + 1).toString() + ' of ' + data.response.length + ')');
+    //alert('Downloading ' + uri + ' to ' + localPath);
 
     fileTransfer.download(
         uri,
         localUrl,
         function(entry) {
-            alert('download complete (path): ' + entry.fullPath); // Returns '/vids/some_video.mp4'
-            alert('download complete (url): ' + entry.toURL()); // Returns 'cdvfile://localhost/persistent/vids/some_video.mp4'
+            //alert('download complete (path): ' + entry.fullPath); // Returns '/vids/some_video.mp4'
+            //alert('download complete (url): ' + entry.toURL()); // Returns 'cdvfile://localhost/persistent/vids/some_video.mp4'
             /*
             document.getElementById('video_container').innerHTML = 
             'Downloaded Video path: ' + entry.fullPath + '<br />'
@@ -108,14 +90,24 @@ function gotFile(fileEntry) {
             + '<source src="' + entry.toURL() + '" type="video/mp4">'
             + '</video>';
             */
-            alert('Wrote video player');
+            data.response[downloadCount].filepath = entry.toURL();
+            downloadCount++;
+            if (downloadCount < data.response.length) {
+              window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+            } else {
+              localStorage.setItem('data', JSON.stringify(data));
+              alert(localStorage.getItem('data'));
+              alert('Videos successfully downloaded!');
+              document.location = 'main.html';
+            }
         },
         function(error) {
-            alert('download error source ' + error.source);
-            alert('download error target ' + error.target);
+            //alert('download error source ' + error.source);
+            //alert('download error target ' + error.target);
+            alert('There was an error while downloading the videos.  Please reopen the application and try again.');
+            navigator.app.exitApp();
         }
     );
-    alert('got');
 }
 
 function fail(error) {
